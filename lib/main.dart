4fcde558d280470d88b52a9376fa6775da028563
ETAvadh <developer.empiere@gmail.com>
2025-03-08 14:08:46 +0530

    - Initialized the Firebase in the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:login_form_validation/app_bloc_observer.dart';
 import 'package:login_form_validation/bloc/bloc_auth.dart';
+import 'package:login_form_validation/firebase_options.dart';
 import 'package:login_form_validation/palette.dart';
 import 'package:login_form_validation/screen_login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
